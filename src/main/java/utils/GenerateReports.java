c92ea54139ef00f863443c759549ed7d4d346442
Brijendra Singh <bps.brijendra@gmail.com>
2018-08-12 19:19:02 +0530

    cucumber report file added- masterthought

---
+++ src/main/java/utils/GenerateReports.java
@@ -0,0 +1,57 @@
+package utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
+
+public class GenerateReports {
+
+	static List<String> jsonFiles(String directory) {
+		List<String> jsonFiles = new ArrayList<>();
+		File dir = new File(directory);
+		for (File file : dir.listFiles()) {
+			if (file.getName().endsWith(".json")) {
+				jsonFiles.add("target/json/" + file.getName());
+			}
+		}
+
+		return jsonFiles;
+	}
+
+	public static void main(String[] args) {
+
+		File reportOutputDirectory = new File("target/cucumberReports");
+		List<String> jsonFiles = jsonFiles("target/json/");
+		
+		String buildNumber = "1";
+		String projectName = "Brijendra cucumberProject";
+		boolean skippedFails =true;
+		boolean pendingFails =false;
+		boolean undefinedFails = true;
+		boolean missingFails =true;
+		boolean runWithJenkins = false;
+		boolean parallelTesting = false;
+
+		
+		Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+		
+		//configuration.setStatusFlags(skippedFails, pendingFails, undefinedFails, missingFails);
+		configuration.setRunWithJenkins(runWithJenkins);
+		configuration.setBuildNumber(buildNumber);
+		//configuration.setJenkinsBasePath("");
+		configuration.setParallelTesting(false);
+		
+		configuration.addClassifications("Platform", "Windows");
+		configuration.addClassifications("Browser", "Firefox");
+		configuration.addClassifications("Branch", "release/1.0");
+		
+		ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+		Reportable result = reportBuilder.generateReports();
+	
+	}
+
+}
